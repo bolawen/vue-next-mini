d23149d70e7875f9ab37d347e37d3c82c9f76364
bolawen <zwq18235130928@163.com>
2023-06-15 08:13:57 +0800

    推送

--- test/class.js
+++ test/class.js
@@ -1,17 +1,55 @@
-class RefImpl {
-  constructor(value) {
-    this._value = value;
-  }
-  get value() {
-    console.log('访问');
-    return this._value;
-  }
-  set value(newValue) {
-    console.log('设置');
-    this._value = newValue;
+// function Point(x, y) {
+//   this.x = x;
+//   this.y = y;
+// }
+
+// Point.prototype.getX = function () {
+//   return this.x;
+// };
+// Point.prototype.setX = function (value) {
+//   this.x = value;
+// };
+// Point.prototype.toString = function () {
+//   return `(${this.x},${this.y})`;
+// };
+
+// const point = new Point(10, 10);
+// console.log(point.toString());
+// point.setX(20);
+// console.log(point.getX());
+
+// function Point(name) {
+//   console.log(new.target);
+// }
+
+// Point(); // undefined
+
+// const point = new Point(); // Point
+
+class Point {
+  constructor() {
+    if (new.target === Point) {
+      throw new Error('本类不能实例化');
+    }
   }
 }
 
-const ref = new RefImpl(5);
-console.log(ref.value);
-ref.value = 4;
+class ThreePoint extends Point {
+  constructor() {
+    super();
+  }
+}
+
+// const point = new Point();
+const threePoint = new ThreePoint();
+
+// function Point() {
+//   if (new.target !== Point) {
+//     console.log('必须通过 new 调用');
+//     return;
+//   }
+//   console.log('new 调用之后');
+// }
+
+// Point(); // 必须通过 new 调用
+// const point = new Point(); // new 调用之后
